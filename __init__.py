0440474343e5d37ae39d27a135a16212659170ed
Jack English <jackjenglish@gmail.com>
2022-01-18 19:15:09 +0000

    adjust sobel

--- __init__.py
+++ __init__.py
@@ -152,7 +152,7 @@ def contrast_loss_edge(input, weight = 1, contrast_diff_weight = 1.25):
 """
 
 def contrast_loss_edge(input, weight = 1, contrast_diff_weight = 1.25):
-  sobel_mask = kornia.filters.sobel(input)
+  sobel_mask = kornia.filters.sobel(input) + 1
   print('sobel', torch.amin(sobel_mask), torch.amax(sobel_mask))
   adjusted = (sobel_mask * (input - 0.5)) + 0.5
   adjusted = torch.clamp(adjusted, min=0, max=1)
