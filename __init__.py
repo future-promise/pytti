0b37c449b44a2fa65033bef48575af713da83341
Jack English <jackjenglish@gmail.com>
2022-01-18 17:38:52 +0000

    sobel mask eg

--- __init__.py
+++ __init__.py
@@ -138,6 +138,7 @@ def contrast_loss_edge(input, weight = 1, contrast_diff_weight = 1.25):
   sobel_mask_converted = 1 + (1 * sobel_mask_clamped)
   sobel_mask_converted = nn.functional.pad(sobel_mask_converted, (1,1,1,1))
   print('sobel shapes', sobel_mask_converted.shape, input.shape)
+  print('sobel mask eg', sobel_mask_converted.squeeze()[30])
   adjusted = (sobel_mask_converted * (input - 0.5)) + 0.5
   adjusted = torch.clamp(adjusted, min=0, max=1)
 
